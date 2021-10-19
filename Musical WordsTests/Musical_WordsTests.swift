65e80739ca8a51e25ab728e43e7241be5f3a3868
Adam Byron <abyron@nevada.unr.edu>
2021-10-19 13:44:43 -0700

    Initial Commit

---
+++ Musical WordsTests/Musical_WordsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Musical_WordsTests.swift
+//  Musical WordsTests
+//
+//  Created by Adam Byron on 10/19/21.
+//
+
+import XCTest
+@testable import Musical_Words
+
+class Musical_WordsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
